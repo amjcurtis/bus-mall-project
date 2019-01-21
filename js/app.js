39d94aa37d15b47e0e80d65b4a13d0a2b47cbd9a
amjcurtis <curtis.andrewm@gmail.com>
2019-01-21 15:22:38 -0800

    Add initial HTML scaffolding and basic JS pseudocode outline

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'user strict';
+
+// Global variables
+
+// Constructor function
+
+// Declare instances using constructor
+
+// Function to show random image
+    
+    // Logic to prevent displaying of duplicate images in set of three
+    
+    // Logic to prevent display of any image twice in consecutive image sets
+
+// Call function to show first set of 3 random images
+
+// Event listener
+
+// Event handler
+    // Validate user input
+
+// Render results to HTML list
